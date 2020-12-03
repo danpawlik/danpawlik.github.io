36471f1efb8afda00244459f2c2e47caccf3c59f
Daniel Pawlik <dpawlik@redhat.com>
2020-12-03 08:38:58 +0100

    Test: adding siteConfig.js

---
+++ my-web-page/siteConfig.js
@@ -0,0 +1,7 @@
+headerIcon: 'img/docusaurus.svg',
+headerLinks: [
+  { doc: "doc1", label: "Getting Started" },
+  { page: "help", label: "Help" },
+  { href: "https://github.com/", label: "GitHub" },
+  { blog: true, label: "Blog" },
+],
